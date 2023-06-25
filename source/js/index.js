3c60af025a73bfef19e321599a07e2bd464a7d4d
zCandev <90929968+zCandev@users.noreply.github.com>
2023-06-25 16:09:36 -0300

    Add files via upload

---
+++ source/js/index.js
@@ -0,0 +1,17 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imgens/seya-noite.jpg");
+    } else {
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imgens/seya-noite.jpg");
+    }
+
+})
